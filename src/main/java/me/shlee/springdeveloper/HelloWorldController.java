0a40427c4f02e848ed17155bf953fcdb6e8fa5e2
shlee1106 <hee5254007@naver.com>
2026-09-03 11:41:16 +0900

    first commit

---
+++ src/main/java/me/shlee/springdeveloper/HelloWorldController.java
@@ -0,0 +1,44 @@
+package me.shlee.springdeveloper;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+    // "/hello" 요청을 보내면 hello() 메서드 호출
+    // http://localhost:8080/hello
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Controller Hello";
+    }
+
+    @GetMapping("/test")
+    // @ResponseBody
+    public String test() {
+
+        return "Hello Everyone!!!!!!!";
+    }
+
+    @PostMapping("/test")
+    public String postTest() {
+        return "Post Test response!!!!!";
+    }
+
+    @DeleteMapping("/test")
+    public String deleteTest() {
+        return "Delete test response!!!!";
+    }
+
+    @PutMapping("/test")
+    public String putTest() {
+        return "Put test response!!!!";
+    }
+
+    @PatchMapping("/test")
+    public String patchTest() {
+        return "Patch test response!!!!";
+    }
+}
---
+++ src/main/java/me/shlee/springdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,15 @@
+package me.shlee.springdeveloper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+
+    public static void main(String[] args) {
+        // 스프링 부트 실행.
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
